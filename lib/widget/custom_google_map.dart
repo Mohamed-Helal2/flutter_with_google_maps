76c4d90bd3b38ac013a1b24fae0404f531d44f1d
mohamedhelal <mhelal566@yahoo.com>
2024-07-28 14:36:24 +0300

    change map type

--- lib/widget/custom_google_map.dart
+++ lib/widget/custom_google_map.dart
@@ -30,6 +30,7 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
     return Stack(
       children: [
         GoogleMap(
+          mapType: MapType.normal,
           onMapCreated: (controller) {
             googleMapController = controller;
           },
